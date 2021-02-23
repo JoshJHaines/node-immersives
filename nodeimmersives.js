c1cb6634537a4903b6a92a80208638f39bfa3246
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-23 15:34:45 -0500

    created variables for first and last name. Tested with console log that it was returning expected results. Created greating.

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,2 +1,11 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
+
+const first = getInput(0);
+const last = getInput(1);
+
+console.log("---------------------------------------------------");
+console.log(first);
+console.log(last);
+console.log("Hello " + first + " " + last + "! Thanks for using my app!"
+console.log("---------------------------------------------------");
