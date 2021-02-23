1c70852f6b9abe38419d40930508e237f5cbd418
JoshuaJHaines <joshjhaines3@gmail.com>
2021-02-23 16:10:29 -0500

    changed variables to be more descriptive

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,12 +1,12 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
 
-const first = getInput(0);
-const last = getInput(1);
+const firstName = getInput(0);
+const lastName = getInput(1);
 
 console.log("---------------------------------------------------");
-console.log("Hello " + first + " " + last + "! Thanks for using my app!");
-console.log("Let's scream your name! " + first.toUpperCase() + " " + last.toUpperCase());
-console.log("Now here are your initials! " + first[0] + "." + last[0] + ".")
-console.log("Finally here is your new email address! " + first.toLowerCase() + "." + last.toLowerCase() + "@codeimmersives.com")
+console.log("Hello " + firstName + " " + lastName + "! Thanks for using my app!");
+console.log("Let's scream your name! " + firstName.toUpperCase() + " " + lastName.toUpperCase());
+console.log("Now here are your initials! " + firstName[0] + "." + lastName[0] + ".")
+console.log("Finally here is your new email address! " + firstName.toLowerCase() + "." + lastName.toLowerCase() + "@codeimmersives.com")
 console.log("---------------------------------------------------");
